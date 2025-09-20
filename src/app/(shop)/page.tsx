43d6b5a559451ea2aecb428e62ffef29b59958c9
Michael Jola-Moses <jolamoses@gmail.com>
2025-09-20 19:52:09 +0100

    fix buildtime error

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,29 +1,14 @@
 import { DEFAULT_PAGE_SIZE } from "@/constants/api";
 import { HomeView } from "@/modules/home/ui/views/home-view";
-import { HydrateClient, caller, getQueryClient } from "@/trpc/server";
+import { HydrateClient, prefetch, trpc } from "@/trpc/server";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 export default async function Home() {
-  const queryClient = getQueryClient();
-
-  // Use server caller directly to avoid HTTP requests during SSR
-  await Promise.all([
-    queryClient.prefetchQuery({
-      queryKey: [["categories", "getAll"], { type: "query" }],
-      queryFn: () => caller.categories.getAll(),
-    }),
-    queryClient.prefetchInfiniteQuery({
-      queryKey: [
-        ["products", "getNewArrivals"],
-        { input: { limit: DEFAULT_PAGE_SIZE }, type: "infinite" },
-      ],
-      queryFn: () =>
-        caller.products.getNewArrivals({ cursor: 0, limit: DEFAULT_PAGE_SIZE }),
-      initialPageParam: 0,
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-    }),
-  ]);
+  prefetch(trpc.categories.getAll.queryOptions());
+  prefetch(
+    trpc.products.getNewArrivals.queryOptions({ limit: DEFAULT_PAGE_SIZE }),
+  );
 
   return (
     <HydrateClient>
