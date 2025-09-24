6e95b8686b3c890b00f983548b7b83abfed51bda
Michael Jola-Moses <jolamoses@gmail.com>
2025-09-24 18:54:50 +0100

    fix buildtime error

--- src/modules/orders/ui/views/orders-view.tsx
+++ src/modules/orders/ui/views/orders-view.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { ShopPageHeader } from "@/components/shared/shop-page-header";
+import { Spinner2 } from "@/components/shared/spinner-2";
 import { useTRPC } from "@/trpc/client";
-import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { OrdersList } from "../components/orders-list";
 
 export const OrdersView = () => {
   const trpc = useTRPC();
 
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useSuspenseInfiniteQuery({
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
+    useInfiniteQuery({
       ...trpc.orders.getUserOrders.infiniteQueryOptions(
         {
           limit: 20,
@@ -20,6 +21,22 @@ export const OrdersView = () => {
       ),
       select: (data) => data.pages.flatMap((page) => page.items),
     });
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center gap-2">
+        <Spinner2 /> Loading...
+      </div>
+    );
+  }
+
+  if (!data) {
+    return (
+      <div className="flex items-center justify-center gap-2">
+        <p>No data available</p>
+      </div>
+    );
+  }
 
   return (
     <div className="space-y-4 p-4">
