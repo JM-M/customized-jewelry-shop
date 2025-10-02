5d0e3acca11d2ffe6954de9153985a711d4332b4
Michael Jola-Moses <jolamoses@gmail.com>
2025-10-02 12:22:19 +0100

    fix buildtime error

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -3,7 +3,8 @@ import { Footer } from "@/components/footer";
 import { MainNavbar } from "@/components/main-navbar";
 import { MainSidebar } from "@/components/main-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { prefetch, trpc } from "@/trpc/server";
+import { HydrateClient, prefetch, trpc } from "@/trpc/server";
+import { Suspense } from "react";
 import { ShopProviders } from "./shop-providers";
 
 type Props = {
@@ -13,17 +14,21 @@ const ShopLayout = async ({ children }: Props) => {
   prefetch(trpc.categories.getAll.queryOptions());
 
   return (
-    <ShopProviders>
-      <CartSheet />
-      <SidebarProvider>
-        <MainSidebar />
-        <div className="flex min-h-screen max-w-full flex-1 flex-col">
-          <MainNavbar />
-          <main className="flex-1">{children}</main>
-        </div>
-      </SidebarProvider>
-      <Footer />
-    </ShopProviders>
+    <HydrateClient>
+      <Suspense fallback={<></>}>
+        <ShopProviders>
+          <CartSheet />
+          <SidebarProvider>
+            <MainSidebar />
+            <div className="flex min-h-screen max-w-full flex-1 flex-col">
+              <MainNavbar />
+              <main className="flex-1">{children}</main>
+            </div>
+          </SidebarProvider>
+          <Footer />
+        </ShopProviders>
+      </Suspense>
+    </HydrateClient>
   );
 };
 export default ShopLayout;
